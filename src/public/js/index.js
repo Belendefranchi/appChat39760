7dee895e2e0ea89de38765fe8db8472f1f880a38
Belen De Franchi <belendefranchi@gmail.com>
2023-04-25 19:56:48 -0300

    chat en proceso

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,24 @@
 const socket = io();
+
+/* Swal.fire({
+    title: 'Bienvenido!',
+    text: 'Saludos desde el servidor!',
+    icon: 'success',
+    confirmButtonText: 'Cool'
+}); */
+
+let user;
+const chatbox = document.getElementById('chatBox');
+
+Swal.fire({
+    title: 'Ingrese su nombre',
+    input: 'text',
+    text: 'Ingresa el usuario con el que quieres chatear',
+    inputValidator: (value) => {
+        return !value && 'Necesitas ingresar un nombre de usuario para chatear!';
+    },
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+}).then((result) => {
+    user = result.value;
+});
